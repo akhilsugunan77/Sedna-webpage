72c72a98c986680eb4468bd33c84eeeee72dc25e
akhilsugunan77 <104181198+akhilsugunan77@users.noreply.github.com>
2022-08-03 12:50:27 +0530

    start

---
+++ gulpfile.js
@@ -0,0 +1,27 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const browserSync =require("browser-sync").create();
+
+// css
+
+function style(){
+    return gulp.src("./src/scss/**/*.scss")
+            .pipe(sass())
+            .pipe(gulp.dest("./src/css"))
+            .pipe(browserSync.stream());
+}
+
+function watch(){
+    browserSync.init({
+        server:{
+            baseDir:"./src"
+        }
+    })
+    gulp.watch("./src/scss/**/*.scss",style);
+    gulp.watch("./src/**/*.html").on("change", browserSync.reload)
+
+}
+
+
+exports.style = style;
+exports.watch = watch;
