dd097865b9a75f7973e0453532f0899013ca58c1
akhilsugunan77 <104181198+akhilsugunan77@users.noreply.github.com>
2022-08-09 12:19:49 +0530

    removed assets from src

--- gulpfile.js
+++ gulpfile.js
@@ -2,15 +2,14 @@ const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const browserSync =require("browser-sync").create();
 const cleanCss = require('gulp-clean-css');
-const a = require('gulp-imagemin');
-const cache = require('gulp-cache');
 
 // css
 
 function style(){
     return gulp.src("./src/scss/**/*.scss")
             .pipe(sass().on("error",sass.logError))
-            .pipe(gulp.dest("./src/css"))
+            .pipe(cleanCss())
+            .pipe(gulp.dest("./public/css"))
             .pipe(browserSync.stream());
 }
 
@@ -21,17 +20,10 @@ function watch(){
         }
     })
     gulp.watch("./src/scss/**/*.scss",style);
-    gulp.watch("./public/**/*.html").on("change",browserSync.reload)
+    gulp.watch("./public/*.html").on("change",browserSync.reload)
 }
-
-function buildCss(){
-    return gulp.src("./src/css/**/*.css")
-            .pipe(cleanCss())
-            .pipe(gulp.dest('public/css'))
-};
-
 
 
 exports.style = style;
 exports.watch = watch;
-exports.buildCss = buildCss;
+
